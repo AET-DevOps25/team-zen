aaba1dfa0fc082a6c7aa5495c3f132ec6d15996a
Zexin Gong <zexin.gong@tum.de>
2025-05-22 15:20:58 +0200

    feat: Enhance API Gateway with CORS and Circuit Breaker configurations; add user and journal microservice properties

--- server/api-gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
+++ server/api-gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @SpringBootApplication
 @Controller
 public class ApiGatewayApplication {
-
     
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
