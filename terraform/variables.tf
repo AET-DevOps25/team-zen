63540191f6f63bc4d050cd58cdc41b26a410fc9b
crustypluto19 <evanchristopher1907@gmail.com>
2025-05-30 15:58:30 +0200

    fix: simplify Terraform apply command and add default values for variables
    Signed-off-by: crustypluto19 <evanchristopher1907@gmail.com>

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,7 @@
 variable "ami_id" {
   description = "The AMI ID to use for the instance."
   type        = string
+  default     = "ami-084568db4383264d4" // AMI ID for Ubuntu 20.04 in us-east-1
 }
 
 variable "instance_type" {
@@ -12,6 +13,7 @@ variable "instance_type" {
 variable "key_name" {
   description = "SSH key pair name"
   type        = string
+  default     = "vockey" // Key pair name setup from AWS Academy Lab
 }
 
 variable "region" {
