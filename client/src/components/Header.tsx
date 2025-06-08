a3367d57233d8176bf98125b7dd6f1f1964db31b
Zexin Gong <zexin.gong@tum.de>
2025-06-08 18:57:29 +0200

    feat: add API testing button for testing auth

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,12 +3,42 @@ import {
   SignedIn,
   SignedOut,
   UserButton,
+  useAuth,
 } from '@clerk/clerk-react';
 import { Link } from '@tanstack/react-router';
 import { TreeDeciduousIcon } from 'lucide-react';
 import { Button } from './ui/button';
 
 export default function Header() {
+  const {getToken} = useAuth();
+
+  const getUsers = async () => {
+    try {
+        const token = await getToken();
+        console.log('Token:', token);
+        const response = await fetch('http://localhost:8085/api/users', {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${token}`,
+            Accept: 'application/json',
+          },
+        }
+        );
+
+        if (response.status == 401) {
+          alert('Authentication failed. Please sign in.');
+          return;
+        } else if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        console.log('Users:', data);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  };
+
   return (
     <header className="py-4 flex bg-white text-black justify-between">
       <nav className="flex flex-row">
@@ -23,6 +53,7 @@ export default function Header() {
         </div>
       </nav>
       <div className="flex items-center gap-2">
+        <Button onClick={getUsers}>Test API</Button>
         <SignedOut>
           <Button asChild>
             <SignInButton />
