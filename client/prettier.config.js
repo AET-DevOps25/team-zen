0d66c6a4b079d80f33432b7462faa6758bbc0229
crustypluto19 <evanchristopher1907@gmail.com>
2025-06-09 21:18:52 +0200

    fix: lint in client and path for genai server
    Signed-off-by: crustypluto19 <evanchristopher1907@gmail.com>

--- client/prettier.config.js
+++ client/prettier.config.js
@@ -5,9 +5,6 @@ const config = {
   semi: true,
   singleQuote: true,
   trailingComma: 'all',
-  importOrder: ['^components/(.*)$', '^[./]'],
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
 };
 
 export default config;
