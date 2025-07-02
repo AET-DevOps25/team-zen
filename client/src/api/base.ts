f2ba840092579d83268abdb95e5b56c98ca5fcac
Natalia Milanova <ge65was@mytum.de>
2025-07-02 20:43:53 +0200

    fix env frontend

--- client/src/api/base.ts
+++ client/src/api/base.ts
@@ -1,4 +1,7 @@
-export const API_BASE_URL = import.meta.env.API_URL || 'http://localhost:8085';
+const rawUrl = "__VITE_API_URL__";
+// TODO: I dont know why and how is this used for
+export const API_BASE_URL = rawUrl !== "__VITE_API_URL__" ? rawUrl : 'http://localhost:8085';
+// export const API_BASE_URL = import.meta.env.API_URL || 'http://localhost:8085';
 
 export type ApiResponse<T> = {
   data: T;
--- client/src/env.ts
+++ client/src/env.ts
@@ -1,23 +1,30 @@
-import { z } from 'zod';
+// import { z } from 'zod';
+//
+// const envSchema = z.object({
+//   VITE_CLERK_PUBLISHABLE_KEY: z
+//     .string()
+//     .min(1, 'Clerk publishable key is required'),
+//   VITE_API_URL: z.string().url(),
+//   // VITE_FEATURE_FLAG: z
+//   //   .enum(['true', 'false'])
+//   //   .transform((val) => val === 'true'),
+// });
+//
+// const _env = envSchema.safeParse(import.meta.env);
+//
+// if (!_env.success) {
+//   console.error(
+//     '❌ Invalid environment variables:',
+//     _env.error.flatten().fieldErrors,
+//   );
+//   throw new Error('Invalid environment variables');
+// }
+//
+// export const env = _env.data;
 
-const envSchema = z.object({
-  VITE_CLERK_PUBLISHABLE_KEY: z
-    .string()
-    .min(1, 'Clerk publishable key is required'),
-  VITE_API_URL: z.string().url(),
-  // VITE_FEATURE_FLAG: z
-  //   .enum(['true', 'false'])
-  //   .transform((val) => val === 'true'),
-});
+// TODO: Validation is removed because these variables are set at runtime for docker and kubernetes.
 
-const _env = envSchema.safeParse(import.meta.env);
-
-if (!_env.success) {
-  console.error(
-    '❌ Invalid environment variables:',
-    _env.error.flatten().fieldErrors,
-  );
-  throw new Error('Invalid environment variables');
-}
-
-export const env = _env.data;
+export const env = {
+  VITE_CLERK_PUBLISHABLE_KEY: "__VITE_CLERK_PUBLISHABLE_KEY__",
+  VITE_API_URL: "__VITE_API_URL__",
+};
