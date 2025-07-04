e585385de31d0fe681bb93d8cd437522119a2f22
Natalia Milanova <ge65was@mytum.de>
2025-07-04 16:28:18 +0200

    add logs

--- client/src/api/snippet.ts
+++ client/src/api/snippet.ts
@@ -62,6 +62,9 @@ export const useGetSnippets = (params: GetSnippetsParams = {}) => {
     }
 
     const queryString = urlParams.toString();
+
+    console.log("VITE_API_URL in fetchSnippets:", env.VITE_API_URL);
+    console.log("API_BASE_URL in fetchSnippets:", API_BASE_URL);
     const url = `${API_BASE_URL}/api/snippets/${user?.id}${queryString ? `?${queryString}` : ''}`;
 
     const response = await fetch(url, {
