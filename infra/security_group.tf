0f6f7b823eb473f1e001431472e8715e12f252de
crustypluto19 <evanchristopher1907@gmail.com>
2025-07-18 18:57:20 +0200

    refactor: enhance Docker login process in playbook.yml; update security group rules in security_group.tf; modify VPC configuration in vpc.tf
    Signed-off-by: crustypluto19 <evanchristopher1907@gmail.com>

--- infra/security_group.tf
+++ infra/security_group.tf
@@ -3,6 +3,7 @@ resource "aws_security_group" "allow_ssh_http" {
   description = "Allow SSH and HTTP access"
   vpc_id      = aws_vpc.main.id
 
+  # SSH access
   ingress {
     from_port   = 22
     to_port     = 22
@@ -10,9 +11,42 @@ resource "aws_security_group" "allow_ssh_http" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # HTTP access (nginx reverse proxy)
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Client application (direct access)
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # API Gateway (direct access)
+  ingress {
+    from_port   = 8085
+    to_port     = 8085
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Grafana dashboard
+  ingress {
+    from_port   = 3001
+    to_port     = 3001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Prometheus monitoring
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
--- infra/vpc.tf
+++ infra/vpc.tf
@@ -1,11 +1,13 @@
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
 resource "aws_subnet" "main" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.region}b"
   map_public_ip_on_launch = true
 }
 
